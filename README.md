1bcb60a584d47a8293553836387249c41156b015
hedeben <benhede@pm.me>
2023-03-23 15:10:57 +0100

    Added options for Caps Lock key binding.

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ A Swedish Colemak DH layout for the /usr/share/X11/xkb/symbols/se file.
 If installed manually, the layout variant needs to be added to /usr/share/X11/xkb/rules/evdev.xml.
 
 To install using the script: 
+    WARNING: Running this multiple times will probably cause problems. And please backup your files before you do.
 
 ```
-wget https://raw.githubusercontent.com/hedeben/swolemak/main/swolemak.sh && chmod +x ./swolemak.sh
-sh ./swolemak.sh
+wget https://raw.githubusercontent.com/hedeben/swolemak/main/swolemak.sh
+sudo sh ./swolemak.sh
 ```
--- swolemak.sh
+++ swolemak.sh
@@ -8,7 +8,7 @@ cat << EOF >> /usr/share/X11/xkb/symbols/se
 
 // Swedish Colemak-DH
 partial alphanumeric_keys
-xkb_symbols "swolemak" {
+xkb_symbols "swolemakdh" {
 
     include "se(basic)"
 
@@ -19,33 +19,53 @@ xkb_symbols "swolemak" {
 //   a r s t g m n e i o ä '
 //  z x c d v < k h , . - 
 
-    key <AD03>	{ [         f,          F,              dstroke,          ordfeminine ]	};
-    key <AD04>	{ [         p,          P,                thorn,                THORN ]	};
+    key <AD03>	{ [         f,          F,              dstroke,          ordfeminine ] };
+    key <AD04>	{ [         p,          P,                thorn,                THORN ] };
     key <AD05>	{ [         b,          B,  leftdoublequotemark,  leftsinglequotemark ] };
     key <AD06>	{ [         j,          J,            dead_hook,            dead_horn ] };
-    key <AD07>	{ [         l,          L,              lstroke,              Lstroke ]	};
-    key <AD08>	{ [         u,          U,            downarrow,              uparrow ]	};
-    key <AD09>	{ [         y,          Y,            leftarrow,                  yen ]	};
-    key <AD10>	{ [odiaeresis, Odiaeresis,               oslash,               Oslash ]	};
+    key <AD07>	{ [         l,          L,              lstroke,              Lstroke ] };
+    key <AD08>	{ [         u,          U,            downarrow,              uparrow ] };
+    key <AD09>	{ [         y,          Y,            leftarrow,                  yen ] };
+    key <AD10>	{ [odiaeresis, Odiaeresis,               oslash,               Oslash ] };
     
-    key <AC02>	{ [         r,          R,           registered,           registered ]	};
-    key <AC03>	{ [         s,          S,               ssharp,                U1E9E ]	};
-    key <AC04>	{ [         t,          T,                thorn,                THORN ]	};
-    key <AC06>	{ [         m,          M,                   mu,            masculine ]	};
-    key <AC07>	{ [         n,          N, rightdoublequotemark, rightsinglequotemark ]	};
-    key <AC08>	{ [         e,          E,             EuroSign,                 cent ]	};
-    key <AC09>	{ [         i,          I,           rightarrow,             idotless ]	};
-    key <AC10>	{ [         o,          O,                   oe,                   OE ]	};
+    key <AC02>	{ [         r,          R,           registered,           registered ] };
+    key <AC03>	{ [         s,          S,               ssharp,                U1E9E ] };
+    key <AC04>	{ [         t,          T,                thorn,                THORN ] };
+    key <AC06>	{ [         m,          M,                   mu,            masculine ] };
+    key <AC07>	{ [         n,          N, rightdoublequotemark, rightsinglequotemark ] };
+    key <AC08>	{ [         e,          E,             EuroSign,                 cent ] };
+    key <AC09>	{ [         i,          I,           rightarrow,             idotless ] };
+    key <AC10>	{ [         o,          O,                   oe,                   OE ] };
     
-    key <LSGT>	{ [         z,          Z,        guillemotleft,                 less ]	};
-    key <AB01>	{ [         x,          X,       guillemotright,              greater ]	};
-    key <AB02>	{ [         c,          C,            copyright,            copyright ]	};
-    key <AB03>	{ [         d,          D,                  eth,                  ETH ]	};
-    key <AB05>  { [      less,    greater,                  bar,            brokenbar ] };
-    key <AB06>	{ [         k,          K,                  kra,            ampersand ]	};
-    key <AB07>	{ [         h,          H,              hstroke,              Hstroke ]	};
+    key <LSGT>	{ [         z,          Z,        guillemotleft,                 less ] };
+    key <AB01>	{ [         x,          X,       guillemotright,              greater ] };
+    key <AB02>	{ [         c,          C,            copyright,            copyright ] };
+    key <AB03>	{ [         d,          D,                  eth,                  ETH ] };
+    key <AB05>	{ [      less,    greater,                  bar,            brokenbar ] };
+    key <AB06>	{ [         k,          K,                  kra,            ampersand ] };
+    key <AB07>	{ [         h,          H,              hstroke,              Hstroke ] };
 
-    key <CAPS> { [  BackSpace,  BackSpace,            BackSpace,            BackSpace ] };
+EOF
+
+echo "Please choose modifier for the Caps Lock key: \n 1 - Backspace, \n 2 - Ctrl, \n 3 - Esc, \n 4 - No change (Caps Lock)"
+read -p "Choose [1/2/3/4 (default)]: " mod
+
+case ${mod} in
+    "1" )
+        echo "Adding Backspace on Caps Lock key."
+        echo "    key <CAPS>	{ [  BackSpace,  BackSpace,            BackSpace,            BackSpace ] };" >> /usr/share/X11/xkb/symbols/se ;;
+    "2" )
+        echo "Adding Left Control on Caps Lock key."
+        echo "    key <CAPS>	{ [  Control_L,  Control_L,            Control_L,            Control_L ] };" >> /usr/share/X11/xkb/symbols/se
+        echo "    modifier_map Control { <CAPS> };" >> /usr/share/X11/xkb/symbols/se ;;
+    "3" )
+        echo "Adding Esc on Caps Lock key."
+        echo "    key <CAPS>	{ [     Escape,     Escape,               Escape,               Escape ] };" >> /usr/share/X11/xkb/symbols/se ;;
+    * )
+        echo "Defaulting to Caps Lock." ;;
+esac
+
+cat << EOF >> /usr/share/X11/xkb/symbols/se
 
     include "level3(ralt_switch)"
 };
@@ -59,15 +79,8 @@ sed -i '/Swedish (no dead keys)/a \
         </variant>\
         <variant>\
           <configItem>\
-            <name>swolemak</name>\
+            <name>swolemakdh</name>\
             <description>Swedish (Colemak-DH)</description>
 ' /usr/share/X11/xkb/rules/evdev.xml
 
-read -p "Do you want to reboot for the changes to take effect? [y/n]" confirm
-
-if [[ ${confirm} == "y" ]]; then
-    echo "Rebooting..." && sleep 5
-    shutdown -r now
-else
-    echo "Alright."
-fi
+echo "Done! You can now choose Colemak in your keyboard layout config. Happy typing!"
